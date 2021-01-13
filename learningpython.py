25481e5ec9f84a3b4affd307dd1f383ae01825ef
Hector Zaragoza <62188104+hectorzaragoza@users.noreply.github.com>
2021-01-13 14:08:49 -0800

    Review of Day 1

--- learningpython.py
+++ learningpython.py
@@ -1,3 +1,21 @@
 print("Hello, World!")
 
 #first Commit
+
+#The first day of learning python consisted of learning about the print function, concatenating values using the + sign
+# and doubling values using the * sign. We also can print characters from a string using the syntax varvalue [Starting Value:Ending Value: Step Value]
+#You can use negative values to go in reverse order.
+
+#Review of Day 1
+print("Hello, Hector")
+
+FirstNameVariable = ("Hector")
+LastNameVariable = (" Zaragoza")
+
+ConcatStr = FirstNameVariable + LastNameVariable
+print(ConcatStr)
+
+print(ConcatStr [7::2]) #start at character 7, print, go two steps, print, until end.
+
+
+
