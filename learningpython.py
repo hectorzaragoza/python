c18d9996473ee967f5e36723927e70fcd5427ba3
Hector Zaragoza <62188104+hectorzaragoza@users.noreply.github.com>
2021-01-13 14:18:50 -0800

    third push

--- learningpython.py
+++ learningpython.py
@@ -3,7 +3,7 @@ print("Hello, World!")
 #first Commit
 
 #The first day of learning python consisted of learning about the print function, concatenating values using the + sign
-# and doubling values using the * sign. We also can print characters from a string using the syntax varvalue [Starting Value:Ending Value: Step Value]
+# and doubling values using the * sign. STRING SLICING We also can print characters from a string using the syntax varvalue [Starting Value:Ending Value: Step Value]
 #You can use negative values to go in reverse order.
 
 #Review of Day 1
@@ -13,6 +13,9 @@ FirstNameVariable = ("Hector")
 LastNameVariable = (" Zaragoza")
 
 ConcatStr = FirstNameVariable + LastNameVariable
+DoubleVarValue = FirstNameVariable * 2
+print(DoubleVarValue)
+
 print(ConcatStr)
 
 print(ConcatStr [7::2]) #start at character 7, print, go two steps, print, until end.
