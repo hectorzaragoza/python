6613e21971d010f239be993e9b8212b3c3586510
Hector Zaragoza <62188104+hectorzaragoza@users.noreply.github.com>
2021-01-14 19:22:11 -0800

    Back to normal

---
+++ loop.py
@@ -0,0 +1,21 @@
+#loops, help us do things repeatedly
+#For loop, in this condition, do this.
+
+
+
+#for newVariableName in somethingToLoopOver:
+#    code
+#    moreCode
+#Not inside loop anymore
+#    indentedCode will result in error if not inside a funciton.
+
+#range(start,stop,step) -->start, start+step, start + n steps < stop (The stop is up to but not including.)
+#for number in range(0,10,1): #range only takes ints, but you can divide by 10 if you wanted to get decimals.
+#    print(number) #new line is automatically produced with each print()
+
+#for latex in range(10): #assumes start of 0 and step 1
+#    print(latex)
+
+for latexTwo in range(11,20,-1):
+    print(latexTwo)
+
