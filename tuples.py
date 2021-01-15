e422859472c32c1adc9869a9e98b21f230d97977
Hector Zaragoza <62188104+hectorzaragoza@users.noreply.github.com>
2021-01-15 13:09:03 -0800

    Finished Tuples

--- tuples.py
+++ tuples.py
@@ -10,15 +10,45 @@ import pygame
 #intTuples = (10,2)
 #length = len(varName)
 
-screenSizeTuple = (900,700) #(width, height)
-print(screenSizeTuple[0:2])
-print(screenSizeTuple[0])
-print(screenSizeTuple[1])
-print(len(screenSizeTuple))
+#screenSizeTuple = (900,700) #(width, height)
+#print(screenSizeTuple[0:2])
+#print(screenSizeTuple[0])
+#print(screenSizeTuple[1])
+#print(len(screenSizeTuple))
 
 #You can create a tuple with only one element but have to include the ,
-testTuple = ("screen size",) + screenSizeTuple
-print(testTuple) #you can know what your tuple represents by concatenating your tuple with a string descriptor in slot 1
-print(testTuple[0])
-print(len(testTuple))
+#testTuple = ("screen size",) + screenSizeTuple
+#print(testTuple) #you can know what your tuple represents by concatenating your tuple with a string descriptor in slot 1
+#print(testTuple[0])
+#print(len(testTuple))
+
+#Operations on Tuples
+#We can check to see what is inside in Tuples
+
+#print("screen size" in testTuple) #This results in true because the string is included in the Tuple.
+
+#Testing to see a tuple inside a tuple
+Size = (800,600) 
+TestTwo = ("Screen Size",) + (Size,)
+print(TestTwo)
+
+print(TestTwo[0])
+print(TestTwo[1])
+
+print(Size in TestTwo)
+print(TestTwo[1][0]) #This should give us 800 #printing the first value of the size tuple withing the TestTwo var
+print(TestTwo[1][1]) #600
+
+print(TestTwo[1][0])
+#is the same as
+print(Size[0])
+
+for var in TestTwo:
+    print(var)
+
+
+
+
+
+
 
