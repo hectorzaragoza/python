ca51c178a73ef3bd627c434009ce3123ef435371
Hector Zaragoza <62188104+hectorzaragoza@users.noreply.github.com>
2021-01-21 13:24:37 -0800

    started exercise skeleton

---
+++ image_exercise.py
@@ -0,0 +1,20 @@
+import pygame
+pygame.init()
+
+
+width = 900
+height = 700
+screenDimension = (width,height)
+
+screen = pygame.display.set_mode(screenDimension)
+pygame.display.set_caption("Exercise One")
+
+
+finished = False
+while finished != True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+            pygame.quit()
+            sys.exit()
+    pygame.display.flip()
