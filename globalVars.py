cbc02e43e4538a25f4f4951ec0d89ea44e82f78e
Hector Zaragoza <62188104+hectorzaragoza@users.noreply.github.com>
2021-01-22 23:43:37 -0800

    Finished

--- globalVars.py
+++ globalVars.py
@@ -2,6 +2,7 @@
 
 def addOne():
     global y,x #this will make the y variable a global variable
+    #x here is defined as a global variable so x in line 9 is able to be imported and used in this function.
     y = x + 1 #y inside this function is a local variable
     return y #a variable inside a function is only defined for that function.
 
